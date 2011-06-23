bb503d2930e8553a516e7fe9a22c036e6c8adeff
mnyeste <misi@baxter-it.com>
2011-06-23 13:28:54 +0200

    commit before refactor

--- time_machine_data.rb
+++ time_machine_data.rb
@@ -2,34 +2,36 @@ require 'app_log'
 
 class TimeMachineData
 
-  attr_writer :response
+  #attr_writer :response
   attr_reader :data
-
-  @response = Array.new
-  
-  def initialize(response)
+  def initialize(serverUrl=nil, project=nil, metric =nil)
     @logger = Log.getLogger()
-    @response = response
-    parse_sonar_response()
+
+    if !serverUrl.nil? then
+      retrieve(serverUrl, project, metric)
+      parse
+    end
+
   end
 
-  def retrieve_time_machine_data(project, metric)
-    
+  private
+  @response = Array.new
+
+  def retrieve(serverUrl, project, metric)
+
     @logger.info("Retrieving #{metric} metric on #{project}")
-    httpresponse = Net::HTTP.post_form(URI.parse(@serverUrl+'/api/timemachine'),
+    httpresponse = Net::HTTP.post_form(URI.parse(serverUrl+'/api/timemachine'),
     {'resource' => "#{project}", 'metrics' => "#{metric}", 'format' => 'csv'});
     @logger.debug("Got HTTP response: #{httpresponse}")
     @response = CSV.parse(httpresponse.body)
-    @logger.debug("Response body parsed: #{@resopnse.inspect}")
-  
-    parse_sonar_response
+    @logger.debug("Response body parsed: #{@response.inspect}")
+    parse
   end
-  
-  private
-  def parse_sonar_response
+
+  def parse
 
     @logger.debug("Parsing response: #{@response.inspect}")
-    
+
     @data = Hash.new
 
     @response.delete_at(0)
@@ -38,7 +40,7 @@ class TimeMachineData
     @response.each { |r| @data[Date.parse(r[0])] = r[1].to_f }
 
     @logger.debug("Parsed data     : [#{@data.sort.join("; ")}]")
-      
+
   end
 
 end
