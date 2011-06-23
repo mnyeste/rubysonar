540f9176f0e84683288df45bc33ce04616d9e6f2
mnyeste <misi@baxter-it.com>
2011-06-23 10:43:17 +0200

    refactor finished

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'date'
-require 'Log'
-require 'SonarConnector'
-require 'MetricTrend'
+require 'app_log'
+require 'sonar_connector'
+require 'metric_trend'
 
 logger = Log.getLogger()
 
@@ -9,7 +9,7 @@ logger.info("Starting...")
 
 sonar = SonarConnector.new("http://sourcecontrol:8001")
      
-mt = MetricTrend.new('2011-05-01','2011-12-31','com.baxter.pe:price-engine',MetricTrend::WORKDAY)
+mt = MetricTrend.new('2011-05-01','2011-12-31','com.baxter.pe:price-engine','coverage',MetricTrend::WORKDAY)
 
 
 
